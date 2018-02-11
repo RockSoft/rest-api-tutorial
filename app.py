98e9790f36fcbc3aafd52bd5f20ebca0b0ece01a
Peter <peter.leigh@rocksoftwaresolutions.com>
2018-02-11 12:15:08 +0000

    dded some spacing

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
