8b7f8917e6f1dcc0e910065d1f79bc91ed92fe78
Peter <peter.leigh@rocksoftwaresolutions.com>
2018-03-03 18:10:24 +0000

    trying to fix voltage_name vs name issue

--- resources/voltage_type.py
+++ resources/voltage_type.py
@@ -20,8 +20,11 @@ class VoltageType(Resource):
         if VoltageTypeModel.find_by_name(voltage_name):
             return {"message": "A a voltage_type with name '{}' already exists".format(voltage_name)}, 400
 
+        print("Voltage name:".format(voltage_name))
+
         voltage_type = VoltageTypeModel(voltage_name)
-        try
+        
+        try:
             voltage_type.save_to_db()
         except:
             return {'message': 'An error occurred inserting the voltage_type'}, 500 #internal server error
