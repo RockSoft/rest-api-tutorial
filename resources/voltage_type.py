a03295302b26d157b129641a383b69912ea9f5f9
Peter <peter.leigh@rocksoftwaresolutions.com>
2018-03-03 19:10:56 +0000

    trying to fix voltage_name vs name issue

--- resources/voltage_type.py
+++ resources/voltage_type.py
@@ -1,60 +1,35 @@
-from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
-from models.voltage_type import VoltageTypeModel
+from flask_restful import Resource
+from models.store import StoreModel
 
-class VoltageType(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('voltage_name',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+class Store(Resource):
 
-    def get(self, voltage_name):
-        voltage_type = VoltageTypeModel.find_by_name(voltage_name)
-        if voltage_type:
-            return voltage_type.json()
-        return {'message':'voltage_type not found'}, 404
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
 
-    def post(self, voltage_name):
-        if VoltageTypeModel.find_by_name(voltage_name):
-            return {'message': "A voltage_type with name '{}' already exists".format(voltage_name)}, 400
+        if store:
+            return store.json()
+        return {'message': 'Store not found'}, 404
 
-        vt = VoltageTypeModel(voltage_name)
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "Store with name '{}' already exists".format(name)},400
 
+        store = StoreModel(name)
         try:
-            vt.save_to_db()
+            store.save_to_db()
         except:
-            return {'message': 'An error occurred inserting the voltage_type'}, 500 #internal server error
+            return {'message': 'An error occurred while creating the store.'}, 500
 
-        return vt.json(), 201
+        return store.json(), 201
 
-    def delete(self, voltage_name):
-        voltage_type = VoltageTypeModel.find_by_name(voltage_name)
-        if voltage_type:
-            voltage_type.delete_from_db()
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
 
-        return {'message': 'Voltage Type deleted'}
-
-    def put(self, voltage_name):
-        data = VoltageType.parser.parse_args()
-
-        # We first see if the the item already exists by name
-        voltage_type = VoltageTypeModel.find_by_name(voltage_name)
-
-        if voltage_type is None:
-            # Item doesn't exist so we create a new object
-            voltage_type = VoltageTypeModel(voltage_name, **data)
-        else:
-            # item does exist so we just need to update the price & the store id
-            voltage_type.voltage_name = data['voltage_name']
-
-        # The save_to_db() will then either insert or update the database
-        voltage_type.save_to_db()
-
-        return voltage_type.json()
+        return {'message': 'Store deleted'}
 
 
-class VoltageTypeList(Resource):
+class StoreList(Resource):
     def get(self):
-        return {'Voltage Types': [voltage_type.json() for voltage_type in VoltageTypeModel.query.all()]}
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
