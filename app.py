91a8516b3494aceee6dcd4d83ee64562b3184c09
Peter <peter.leigh@rocksoftwaresolutions.com>
2018-03-05 13:06:36 +0000

    Fixing name issue with Neeraj's help

--- app.py
+++ app.py
@@ -11,10 +11,13 @@ from resources.store import Store, StoreList
 from resources.voltage_type import VoltageType, VoltageTypeList
 
 app = Flask(__name__)
+print("This is my database url: %s" % os.environ(DATABASE_URL))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'peter'
 api = Api(app)
+
 
 jwt = JWT(app, authenticate, identity_function) #/auth
 
